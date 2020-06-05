9426698a36b30cf3355ce372fb57a3fe9c2b6967
Bruno Souto <bruno.csouto@gmail.com>
2020-06-05 13:11:04 -0300

    App

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import express, { request } from "express";
+import path from "path";
+import cors from "cors";
+import router from "./routes";
+import { errors } from "celebrate";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+
+app.use(errors());
+
+app.listen(3333);
